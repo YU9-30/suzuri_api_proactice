040de601d4b0d51d8b6bbb772eb4ad2aa42e15cc
yu9-30 <yukyu.konpeki.dou@gmail.com>
2020-07-14 21:48:30 +0900

    自分が作成したプロダクトを取得する

---
+++ test/index.php
@@ -0,0 +1,18 @@
+<?php
+    require ("suzuri_token.php");
+    $base_url = 'https://suzuri.jp/api/v1/products?userName=';
+    $store_name = 'yukyu_konpeki_dou';
+    $curl = curl_init($base_url.$store_name);
+    $option = [
+    CURLOPT_CUSTOMREQUEST => 'GET',
+    CURLOPT_RETURNTRANSFER => true,//curlの結果を自動で表示させない
+    CURLOPT_HTTPHEADER => [
+        'Authorization: Bearer '.$token,  //アクセストークンnmm
+        'Content-Type: application/json',
+        ],
+    ];
+    curl_setopt_array($curl, $option);
+    $result = curl_exec($curl);
+    echo $result;
+    curl_close($curl);
+?>
