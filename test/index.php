badde08ed174e21ebaf1bfdf97a824c04d4248da
YU9-30 <61819079+YU9-30@users.noreply.github.com>
2020-08-18 22:01:06 +0900

    コメントを追記

--- test/index.php
+++ test/index.php
@@ -1,13 +1,13 @@
 <?php
-    require ("suzuri_token.php");
+    require ("suzuri_token.php");//別ファイルに保存した，アクセストークンを読み込み
     $base_url = 'https://suzuri.jp/api/v1/products?userName=';
-    $store_name = 'yukyu_konpeki_dou';
+    $store_name = 'yukyu_konpeki_dou';//自分のSUZURIでのID
     $curl = curl_init($base_url.$store_name);
     $option = [
     CURLOPT_CUSTOMREQUEST => 'GET',
     CURLOPT_RETURNTRANSFER => true,//curlの結果を自動で表示させない
     CURLOPT_HTTPHEADER => [
-        'Authorization: Bearer '.$token,  //アクセストークンnmm
+        'Authorization: Bearer '.$token,  //アクセストークン
         'Content-Type: application/json',
         ],
     ];
